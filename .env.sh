56ef78c16ce8738143fba546e01524e8627bbbbc
lmio <2091319361@qq.com>
2023-06-21 09:45:06 +0800

    chore: doc, env

---
+++ .env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/sugar?useUnicode=true&characterEncoding=utf8
+export SPRING_DATASOURCE_USERNAME=root
+export MYSQL_ROOT_PASSWORD=123456
+export MYSQL_DATABASE=sugar
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # 使用docker容器化部署springboot项目
 ## 如何使用
 
+- 使用docker
 ```shell
 docker compose up -d
 ```
+
+- 本地开发
+  - 设置环境变量
+  - 安装redis
+  - 安装mysql，data.sql
